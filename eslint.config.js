7acd285656293005bac2eef553359a0f1a5b1212
Jhony Freitas <jhony00._@hotmail.com>
2025-06-05 19:38:08 -0300

    feat: Init Vite

---
+++ eslint.config.js
@@ -0,0 +1,75 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import eslintPluginImport from "eslint-plugin-import";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      import: eslintPluginImport,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      indent: ["error", 2, { SwitchCase: 1 }],
+      quotes: ["error", "single"],
+      semi: ["error", "always"],
+      "block-spacing": ["error", "always"],
+      "object-curly-spacing": ["error", "always"],
+      curly: ["error", "all"],
+      "no-alert": "error",
+      eqeqeq: ["error", "always"],
+      "react/prop-types": "off",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          pathGroups: [
+            {
+              pattern: "@/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["builtin"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "no-multiple-empty-lines": [
+        "error",
+        {
+          max: 1,
+          maxEOF: 0,
+          maxBOF: 0,
+        },
+      ],
+    },
+  }
+);
