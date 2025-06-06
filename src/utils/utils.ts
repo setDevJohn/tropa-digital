9c5bb21b7698c75aa43dbe3adad23fa0e9cb8322
Jhony Freitas <jhony00._@hotmail.com>
2025-06-05 21:04:11 -0300

    feat: Add Toastify

---
+++ src/utils/utils.ts
@@ -0,0 +1,20 @@
+import { toast } from 'react-toastify';
+import type { ToastPosition } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const position: ToastPosition = 'top-center';
+const theme = 'light';
+
+const toastSuccess = (message: string) => {
+  toast.success(message, { position, theme });
+};
+
+const toastWarn = (message: string) => {
+  toast.warn(message, { position, theme });
+};
+
+const toastError = (message: string) => {
+  toast.error(message, { position, theme });
+};
+
+export { toastSuccess, toastWarn, toastError };
