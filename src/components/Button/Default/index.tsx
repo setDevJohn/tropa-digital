3b27e78e680da2edfdfc3b68c07f5cd5df83f505
Jhony Freitas <jhony00._@hotmail.com>
2025-06-07 01:09:11 -0300

    chore: add  secondary collor

--- src/components/Button/Default/index.tsx
+++ src/components/Button/Default/index.tsx
@@ -1,18 +1,35 @@
+import { StyledImage } from '../../../styles/globalComponents';
+
 import { ButtonContainer } from './styles';
+
+import type { CSSObject } from 'styled-components';
 
 interface IDefaultButton {
   text: string;
   action?: () => void;
   width?: string;
+  icon?: string;
+  secondary?: boolean;
+  style?: CSSObject
 }
 
-export const DefaultButton = ({ text, action, width }: IDefaultButton) => {
+export const DefaultButton = ({ 
+  text,
+  action,
+  width,
+  icon,
+  secondary,
+  style
+}: IDefaultButton) => {
   return (
     <ButtonContainer 
       $width={width}
       onClick={() => action?.()}
+      $secondary={secondary}
       type={action ? 'button' : 'submit'}
+      style={style}
     >
+      {icon && <StyledImage src={icon}/>}
       {text}
     </ButtonContainer>
   );
--- src/components/Button/Default/styles.ts
+++ src/components/Button/Default/styles.ts
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
-export const ButtonContainer = styled.button<{ $width?: string}>`
-  background-color: ${({ theme }) => theme.primaryColor};
-  color: #fff;
+export const ButtonContainer = styled.button<{ $width?: string; $secondary?: boolean}>`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  background-color: ${({ theme, $secondary }) => $secondary ? theme.buttonLightColor : theme.primaryColor};
+  color: ${({ $secondary }) => $secondary ? '#000' : '#fff'};
   font-size: ${({ theme }) => theme.normalSize};;
   font-weight: 500;
   border: none;
