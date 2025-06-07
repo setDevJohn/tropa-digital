cf868af9a89e908dafe3511296d1d444f75fb086
Jhony Freitas <jhony00._@hotmail.com>
2025-06-07 01:39:42 -0300

    feat: Create events page

---
+++ src/pages/Events/index.tsx
@@ -0,0 +1,23 @@
+import optionsIcon from '../../assets/icons/options.svg';
+import { Table } from '../../components/Table';
+import { ActiveComponentTable, Pagination } from '../../components/Table/components';
+import { TableFilters } from '../../components/Table/components/Filters';
+import { StyledImage } from '../../styles/globalComponents';
+
+export const Events = () => {
+  const tableHeader = ['Nome do evento', 'Total de equipes', 'Status', 'Data', ''];
+  const tableBody = [
+    ['Clube do Laço Coração Pantaneiro', '10', <ActiveComponentTable />, '09 a 11 de Junho', <StyledImage src={optionsIcon}/>],
+    ['Clube do Laço Coração Pantaneiro', '10', <ActiveComponentTable />, '09 a 11 de Junho', <StyledImage src={optionsIcon}/>],
+  ];
+
+  return (
+    <>
+      <TableFilters />
+
+      <Table tableHeader={tableHeader} tableBody={tableBody}/>
+      
+      <Pagination />
+    </>
+  );
+};
